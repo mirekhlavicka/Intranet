701b1d476d04241c04bed5de4bc8f41b894c51d2
Miroslav Hlavicka <hlavicka@mf.cz>
2016-10-06 16:53:13 +0200

    Courses finished

--- IntranetPublic/Controllers/CoursesController.cs
+++ IntranetPublic/Controllers/CoursesController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
+using IntranetPublic.Models;
 
 namespace IntranetPublic.Controllers
 {
@@ -11,9 +13,49 @@ namespace IntranetPublic.Controllers
         private PubSystemEntities db = new PubSystemEntities();
 
         [AcceptVerbs(HttpVerbs.Get | HttpVerbs.Post)]
-        public ActionResult Index()
+        public ActionResult Index(int sort = 0, string filter = "", string search = "")
         {
-            return View(db.Courses);
+            var courses = db.Courses.AsQueryable();
+
+            if (!String.IsNullOrEmpty(filter))
+            {
+                courses = courses.Where(c => c.semester == filter);
+            }
+
+            switch (sort)
+            {
+                case 1:
+                    courses = courses.OrderBy(c => c.code);
+                    break;
+                case 2:
+                    courses = courses.OrderByDescending(c => c.semester).ThenBy(c => c.name);
+                    break;
+                default:
+                    courses = courses.OrderBy(c => c.name);
+                    break;
+            }
+
+            var courses1 = courses.AsEnumerable().Select(c => new CourseViewModel(c));
+
+            if (!String.IsNullOrEmpty(search))
+            {
+                courses1 = courses1.Where(d =>
+                            {
+                                bool res = false;
+                                d.CodeSearch = Regex.Replace(d.CodeSearch, search, m => { res = true; return String.Format("<mark>{0}</mark>", m.ToString()); }, RegexOptions.IgnoreCase);
+                                d.Name = Regex.Replace(d.Name, search, m => { res = true; return String.Format("<mark>{0}</mark>", m.ToString()); }, RegexOptions.IgnoreCase);
+                                return res;
+                            });
+            }
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_CourseList", courses1.ToList());
+            }
+            else
+            {
+                return View(courses1.ToList());
+            }
         }
 
         protected override void Dispose(bool disposing)
---
+++ IntranetPublic/Models/CourseViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IntranetPublic.Models
+{
+    public class CourseViewModel
+    {
+        public String Name { get; set; }
+
+        public String Code { get; set; }
+
+        public String CodeSearch { get; set; }
+
+        public String Semester { get; set; }
+
+        public String CVISID { get; set; }
+
+        public CourseViewModel()
+        {
+
+        }
+
+        public CourseViewModel(Cours cours)
+        {
+            Name = cours.name;
+            Code = cours.code.ToUpper();
+            CodeSearch = Code;
+            Semester = cours.semester;
+            CVISID = cours.cvis_id;
+        }
+
+    }
+}
